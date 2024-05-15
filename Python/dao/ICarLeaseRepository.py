4c9149ff95d62e3ba3f3fae297d5d88663f040c7
uiuxmathesh <matheshpremkumar@gmail.com>
2024-05-16 01:05:13 +0530

    Eliminated nested abstraction and Implemented Single interface

--- Python/dao/ICarLeaseRepository.py
+++ Python/dao/ICarLeaseRepository.py
@@ -1,67 +1,65 @@
 from abc import ABC, abstractmethod
 
 class ICarLeaseRepository(ABC):
+    
+    # Car Management
+    @abstractmethod
+    def addCar(self, car) -> None:
+        pass
 
-    class CarManagement:
+    @abstractmethod
+    def removeCar(self, carID):
+        pass
 
-        @abstractmethod
-        def addCar(self, car) -> None:
-            pass
+    @abstractmethod
+    def listAvailableCars(self) -> list:
+        pass
 
-        @abstractmethod
-        def removeCar(self, carID):
-            pass
+    @abstractmethod
+    def listRentedCars(self) -> list:
+        pass
 
-        @abstractmethod
-        def listAvailableCars(self) -> list:
-            pass
+    @abstractmethod
+    def findCarByID(self, carID) -> list:
+        pass
 
-        @abstractmethod
-        def listRentedCars(self) -> list:
-            pass
-
-        @abstractmethod
-        def findCarByID(self, carID) -> list:
-            pass
-
-    class CustomerManagement:
+    # Customer Management
             
-        @abstractmethod
-        def addCustomer(self, customer) -> None:
-            pass
+    @abstractmethod
+    def addCustomer(self, customer) -> None:
+        pass
 
-        @abstractmethod
-        def removeCustomer(self, customerID):
-            pass
+    @abstractmethod
+    def removeCustomer(self, customerID):
+        pass
 
-        @abstractmethod
-        def listCustomers(self) -> list:
-            pass
+    @abstractmethod
+    def listCustomers(self) -> list:
+        pass
 
-        @abstractmethod
-        def findCustomerByID(self, customerID) -> list:
-            pass
+    @abstractmethod
+    def findCustomerByID(self, customerID) -> list:
+        pass
 
-    class LeaseManagement:
+    # Lease Management
         
-        @abstractmethod
-        def createLease(self, lease) -> None:
-            pass
+    @abstractmethod
+    def createLease(self, lease) -> None:
+        pass
 
-        @abstractmethod
-        def returnCar(self, leaseID) -> None:
-            pass
+    @abstractmethod
+    def returnCar(self, leaseID) -> None:
+        pass
 
-        @abstractmethod
-        def listActiveLeases(self) -> list:
-            pass
+    @abstractmethod
+    def listActiveLeases(self) -> list:
+        pass
+    
+    @abstractmethod
+    def listLeaseHistory(self) -> list:
+        pass
 
-        @abstractmethod
-        def listLeaseHistory(self) -> list:
-            pass
-
-    class PaymentManagement:
-
-        @abstractmethod
-        def recordPayment(self, payment) -> None:
-            pass
+    # Payment Management
+    @abstractmethod
+    def recordPayment(self, payment) -> None:
+        pass
--- Python/dao/ICarLeaseRepositoryImpl.py
+++ Python/dao/ICarLeaseRepositoryImpl.py
@@ -10,159 +10,138 @@ class ICarLeaseRepositoryImpl(ICarLeaseRepository):
         self._cursor = self._db_connection.cursor()
     
 
-    class CarManagement:
-
-        def __init__(self):
-            self._db_connection = DBConnUtil().getConnection()
-            self._cursor = self._db_connection.cursor()
-
-        def addCar(self, car:Vehicle) -> None:
-            query = "INSERT INTO [vehicle] ([make],[model],[year],[dailyrate],[status],[passengercapacity],[enginecapacity]) VALUES (?,?,?,?,?,?,?);"
-            values = (car.make, car.model, car.year, car.dailyrate, car.status, car.passengerCapacity, car.engineCapacity)
-            self._cursor.execute(query, values)
-            self._db_connection.commit()
-            query = "SELECT IDENT_CURRENT('vehicle') AS [id]"
-            self._cursor.execute(query)
-            car.id = int(self._cursor.fetchone()[0])
-            self._cursor.close()
-            self._db_connection = DBConnUtil().closeConnection()
-
-        def removeCar(self, carID:int):
-            query1 = "SELECT * FROM [vehicle] WHERE [id] = ?"
-            query2 = "DELETE FROM [vehicle] WHERE [id] = ?"
-            values = (carID,)
-            self._cursor.execute(query1, values)
-            if self._cursor.fetchone() is None:
-                raise CarNotFoundException(f"Car with ID {carID} not found")
-            self._cursor.execute(query2, values)
-            self._db_connection.commit()
-            self._cursor.close()
-            self._db_connection = DBConnUtil().closeConnection()
+    # Car Management
 
 
-        def listAvailableCars(self) -> list:
-            query = "SELECT * FROM [vehicle] WHERE [status] = 'available'"
-            self._cursor.execute(query)
-            result = self._cursor.fetchall()
-            header = tuple([column[0] for column in self._cursor.description])
-            result = [ header ] + result 
-            return result
-
-        def listRentedCars(self) -> list:
-            query = "SELECT * FROM [vehicle] WHERE [status] = 'unavailable'"
-            self._cursor.execute(query)
-            result = self._cursor.fetchall()
-            header = tuple([column[0] for column in self._cursor.description])
-            result = [ header ] + result
-            return result
-
-        def findCarByID(self, carID:int) -> list:
-            query = "SELECT * FROM [vehicle] WHERE [vehicleId] = ?"
-            self._cursor.execute(query, (carID,))
-            result = self._cursor.fetchall()
-            header = tuple([column[0] for column in self._cursor.description])
-            result = [ header ] + result
-            return result
-
-    class CustomerManagement:
+    def addCar(self, car:Vehicle) -> None:
+        query = "INSERT INTO [vehicle] ([make],[model],[year],[dailyrate],[status],[passengercapacity],[enginecapacity]) VALUES (?,?,?,?,?,?,?);"
+        values = (car.make, car.model, car.year, car.dailyrate, car.status, car.passengerCapacity, car.engineCapacity)
+        self._cursor.execute(query, values)
+        self._db_connection.commit()
+        query = "SELECT IDENT_CURRENT('vehicle') AS [id]"
+        self._cursor.execute(query)
+        car.id = int(self._cursor.fetchone()[0])
         
-        def __init__(self):
-            self._db_connection = DBConnUtil().getConnection()
-            self._cursor = self._db_connection.cursor()
 
-        def addCustomer(self, customer:Customer) -> None:
-            query = "INSERT INTO [customer] ([firstname],[lastname],[email],[phoneNumber]) VALUES (?,?,?,?)"
-            values = (customer.firstname, customer.lastname, customer.email, customer.phone)
-            self._cursor.execute(query, values)
-            query = "SELECT IDENT_CURRENT('customer') AS [id]"
-            self._cursor.execute(query)
-            customer.id = int(self._cursor.fetchone()[0])
-            self._db_connection.commit()
-            self._cursor.close()
-            self._db_connection = DBConnUtil().closeConnection()
+    def removeCar(self, carID:int):
+        query1 = "SELECT * FROM [vehicle] WHERE [id] = ?"
+        query2 = "DELETE FROM [vehicle] WHERE [id] = ?"
+        values = (carID,)
+        self._cursor.execute(query1, values)
+        if self._cursor.fetchone() is None:
+            raise CarNotFoundException(f"Car with ID {carID} not found")
+        self._cursor.execute(query2, values)
+        self._db_connection.commit()
 
-        def removeCustomer(self, customerID:int):
-            query = "DELETE FROM [customer] WHERE [customerId] = ?"
-            values = (customerID,)
-            self._cursor.execute(query, values)
-            self._db_connection.commit()
-            self._cursor.close()
-            self._db_connection = DBConnUtil().closeConnection()
-
-        def listCustomers(self) -> list:
-            query = "SELECT * FROM [customer]"
-            self._cursor.execute(query)
-            result = self._cursor.fetchall()
-            header = tuple([column[0] for column in self._cursor.description])
-            result = [ header ] + result
-            return result
-
-        def findCustomerByID(self, customerID:int) -> list:
-            query = "SELECT * FROM [customer] WHERE [customerId] = ?"
-            self._cursor.execute(query, (customerID,))
-            result = self._cursor.fetchall()
-            header = tuple([column[0] for column in self._cursor.description])
-            result = [ header ] + result
-            return result
+    def listAvailableCars(self) -> list:
+        query = "SELECT * FROM [vehicle] WHERE [status] = 'available'"
+        self._cursor.execute(query)
+        result = self._cursor.fetchall()
+        header = tuple([column[0] for column in self._cursor.description])
+        result = [ header ] + result 
+        return result
+    
+    def listRentedCars(self) -> list:
+        query = "SELECT * FROM [vehicle] WHERE [status] = 'unavailable'"
+        self._cursor.execute(query)
+        result = self._cursor.fetchall()
+        header = tuple([column[0] for column in self._cursor.description])
+        result = [ header ] + result
+        return result
+    
+    def findCarByID(self, carID:int) -> list:
+        query = "SELECT * FROM [vehicle] WHERE [vehicleId] = ?"
+        self._cursor.execute(query, (carID,))
+        result = self._cursor.fetchall()
+        header = tuple([column[0] for column in self._cursor.description])
+        result = [ header ] + result
+        return result
+    
+    # Customer Management
         
-    class LeaseManagement:
 
-        def __init__(self):
-            self._db_connection = DBConnUtil().getConnection()
-            self._cursor = self._db_connection.cursor()
+    def addCustomer(self, customer:Customer) -> None:
+        query = "INSERT INTO [customer] ([firstname],[lastname],[email],[phoneNumber]) VALUES (?,?,?,?)"
+        values = (customer.firstname, customer.lastname, customer.email, customer.phone)
+        self._cursor.execute(query, values)
+        query = "SELECT IDENT_CURRENT('customer') AS [id]"
+        self._cursor.execute(query)
+        customer.id = int(self._cursor.fetchone()[0])
+        self._db_connection.commit()
         
-        def createLease(self, lease:Lease) -> None:
-            query = "INSERT INTO [lease] ([customerID],[vehicleID],[startDate],[endDate],[type]) VALUES (?,?,?,?,?)"
-            values = (lease.customerId, lease.vehicleId, lease.startDate, lease.endDate, lease.type)
-            self._cursor.execute(query, values)
-            self._db_connection.commit()
-            query = "SELECT IDENT_CURRENT('lease') AS [id]"
-            self._cursor.execute(query)
-            lease.id = int(self._cursor.fetchone()[0])
-            self._cursor.close()
-            self._db_connection = DBConnUtil().closeConnection()
-
-        def returnCar(self, leaseID:int) -> None:
-            query = "UPDATE [lease] SET [endDate] = ? WHERE [leaseID] = ?"
-            values = (leaseID,)
-            self._cursor.execute(query, values)
-            self._db_connection.commit()
-            self._cursor.close()
-            self._db_connection = DBConnUtil().closeConnection()
-
-        def listActiveLeases(self) -> list:
-            query = "SELECT * FROM [lease] WHERE [endDate] IS NULL"
-            self._cursor.execute(query)
-            result = self._cursor.fetchall()
-            header = tuple([column[0] for column in self._cursor.description])
-            result = [ header ] + result
-            return result
+    def removeCustomer(self, customerID:int):
+        query = "DELETE FROM [customer] WHERE [customerId] = ?"
+        values = (customerID,)
+        self._cursor.execute(query, values)
+        self._db_connection.commit()
         
-        def listLeaseHistory(self) -> list:
-            query = "SELECT * FROM [lease] WHERE [endDate] IS NOT NULL"
-            self._cursor.execute(query)
-            result = self._cursor.fetchall()
-            header = tuple([column[0] for column in self._cursor.description])
-            result = [ header ] + result
-            return result
+    def listCustomers(self) -> list:
+        query = "SELECT * FROM [customer]"
+        self._cursor.execute(query)
+        result = self._cursor.fetchall()
+        header = tuple([column[0] for column in self._cursor.description])
+        result = [ header ] + result
+        return result
+    
+    def findCustomerByID(self, customerID:int) -> list:
+        query = "SELECT * FROM [customer] WHERE [customerId] = ?"
+        self._cursor.execute(query, (customerID,))
+        result = self._cursor.fetchall()
+        header = tuple([column[0] for column in self._cursor.description])
+        result = [ header ] + result
+        return result
         
-        def findLeaseByID(self, leaseID:int) -> list:
-            query = "SELECT * FROM [lease] WHERE [leaseID] = ?"
-            self._cursor.execute(query, (leaseID,))
-            result = self._cursor.fetchall()
-            header = tuple([column[0] for column in self._cursor.description])
-            result = [ header ] + result
-            return result
+    # Lease Management
+    
+    def createLease(self, lease:Lease) -> None:
+        query = "INSERT INTO [lease] ([customerID],[vehicleID],[startDate],[endDate],[type]) VALUES (?,?,?,?,?)"
+        values = (lease.customerId, lease.vehicleId, lease.startDate, lease.endDate, lease.type)
+        self._cursor.execute(query, values)
+        self._db_connection.commit()
+        query = "SELECT IDENT_CURRENT('lease') AS [id]"
+        self._cursor.execute(query)
+        lease.id = int(self._cursor.fetchone()[0])
+    
+    def returnCar(self, leaseID:int) -> None:
+        query = "UPDATE [lease] SET [endDate] = ? WHERE [leaseID] = ?"
+        values = (leaseID,)
+        self._cursor.execute(query, values)
+        self._db_connection.commit()
+        self._cursor.close()
+        self._db_connection = DBConnUtil().closeConnection()
+    def listActiveLeases(self) -> list:
+        query = "SELECT * FROM [lease] WHERE [endDate] IS NULL"
+        self._cursor.execute(query)
+        result = self._cursor.fetchall()
+        header = tuple([column[0] for column in self._cursor.description])
+        result = [ header ] + result
+        return result
+    
+    def listLeaseHistory(self) -> list:
+        query = "SELECT * FROM [lease] WHERE [endDate] IS NOT NULL"
+        self._cursor.execute(query)
+        result = self._cursor.fetchall()
+        header = tuple([column[0] for column in self._cursor.description])
+        result = [ header ] + result
+        return result
+    
+    def findLeaseByID(self, leaseID:int) -> list:
+        query = "SELECT * FROM [lease] WHERE [leaseID] = ?"
+        self._cursor.execute(query, (leaseID,))
+        result = self._cursor.fetchall()
+        header = tuple([column[0] for column in self._cursor.description])
+        result = [ header ] + result
+        return result
 
-    class PaymentManagement:
+    # Payment Management
 
-        def recordPayment(self, payment:Payment) -> None:
-            query = "INSERT INTO [payment] ([leaseID],[amount],[date]) VALUES (?,?,?)"
-            values = (payment.leaseId, payment.amount, payment.paymentDate)
-            self._cursor.execute(query, values)
-            self._db_connection.commit()
-            query = "SELECT IDENT_CURRENT('payment') AS [id]"
-            self._cursor.execute(query)
-            payment.id = int(self._cursor.fetchone()[0])
-            self._cursor.close()
-            self._db_connection = DBConnUtil().closeConnection()
+    def recordPayment(self, payment:Payment) -> None:
+        query = "INSERT INTO [payment] ([leaseID],[amount],[paymentdate]) VALUES (?,?,?)"
+        values = (payment.leaseId, payment.amount, payment.paymentDate)
+        self._cursor.execute(query, values)
+        self._db_connection.commit()
+        query = "SELECT IDENT_CURRENT('payment') AS [id]"
+        self._cursor.execute(query)
+        payment.id = int(self._cursor.fetchone()[0])
+        self._cursor.close()
+        self._db_connection = DBConnUtil().closeConnection()
